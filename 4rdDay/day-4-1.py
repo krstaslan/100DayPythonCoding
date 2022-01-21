e6c76f52bf57737956ac93b8973679c328eac0ff
kursatAslan <krstaslan34@gmail.com>
2022-01-22 00:25:15 +0100

    first commit

---
+++ 4rdDay/day-4-1.py
@@ -0,0 +1,7 @@
+import random
+#I am learning random method
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
---
+++ Day 6/Hurdles Challenge.py
@@ -0,0 +1,2 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Fre
+#ı use this web page to learn looping
