0bb87eae6b390be4371f490a18647b8c949869f4
kursatAslan <krstaslan34@gmail.com>
2022-01-28 03:01:36 +0100

    28.01 update

--- Day 32/birthday-wisher-extrahard-start/main.py
+++ Day 32/birthday-wisher-extrahard-start/main.py
@@ -1,27 +1,67 @@
 ##################### Extra Hard Starting Project ######################
 import pandas
+import random
+import os
+import smtplib
 from datetime import datetime
+
+
+def send_message(to_email, send_to):
+    message = change_letter(send_to)
+    my_email = "kursataslanwarsaw@gmail.com"
+    password = "bagazici123"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=to_email,
+            msg=f"Subject:Happay Birthday\n\n{message}"
+        )
+    print(to_email)
+    print(message)
+
+
+def random_letter():
+    dir = "letter_templates/"
+    path = random.choice(os.listdir("letter_templates"))
+    path = dir + path
+    return path
+
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual
+# name from birthdays.csv
+
+def change_letter(name):
+    path = random_letter()
+    with open(path) as letter:
+        message = letter.read()
+    new_letter = message.replace("[NAME]", name)
+    return new_letter
+
+
 # 1. Update the birthdays.csv
-data=pandas.read_csv("birthdays.csv")
+data = pandas.read_csv("birthdays.csv")
 all_data = data.to_dict()
 
 # 2. Check if today matches a birthday in the birthdays.csv
 now = datetime.now()
-current_month = now.strftime("%m")
-print(current_month)
-current_day = now.strftime("%d")
-if current_month in all_data["month"]:
-    
+current_month = int(now.strftime("%m"))
 
-#for i in all_months:
-#    print(i)
-#    if int(i) == int(current_month):
-#        print("yes")
-#
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+current_day = int(now.strftime("%d"))
+
+name=""
+email=""
+for x in all_data["month"]:
+    try:
+        if current_month == int(all_data["month"][x]) and \
+                current_day == int(all_data["day"][x]):
+            name = all_data["name"][x]
+            email = all_data["email"][x]
+            send_message(email, name)
+
+
+    except ValueError:
+        pass
 
 # 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
